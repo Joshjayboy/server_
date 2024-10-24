e22fc6c5d71925d54e96b4e40bfce9b023ba5c32
joshjayboy <you@example.com>
2024-10-24 05:30:22 +0100

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use(cors());
